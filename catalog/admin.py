4ecb30c71b30e015ec73720918f40c032290fca9
Roman Kuklin <kuklin.prog@gmail.com>
2026-04-18 14:00:25 +0800

    feat: add good model, admin good model

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Good
 
 # Register your models here.
+@admin.register(Good)
+class GoodAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "price", "currency")
+    search_fields = ("name", "description")
--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
 # Create your models here.
+class GoodPriceCurrency(models.TextChoices):
+    USD = "usd", "Доллар (США)"
+    EUR = "eur", "Евро"
+    RUB = "rub", "Российский рубль"
+
+
+class Good(models.Model):
+    name = models.CharField(
+        max_length=255,
+        verbose_name="Название товара"
+    )
+
+    desc = models.TextField(
+        blank=True,
+        verbose_name="Описание товара"
+    )
+
+    price = models.PositiveIntegerField(
+        verbose_name="Цена (в минимальных единицах)",
+        help_text="Цена в копейках/центах"
+    )
+
+    currency = models.CharField(
+        max_length = 3,
+        choices = GoodPriceCurrency.choices,
+        default = GoodPriceCurrency.USD,
+        verbose_name="Валюта"
+    )
+
+    class Meta:
+        verbose_name = "Товар",
+        verbose_name_plural = "Товары"
+
+    def __str__(self):
+        return self.name
