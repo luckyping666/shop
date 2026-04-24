d94d208d9590ede808c6534562f05bed1263803c
Roman Kuklin <kuklin.prog@gmail.com>
2026-04-24 19:14:27 +0800

    feat: add success/cancel payment page

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,11 @@
 from os import name
 from django.urls import path
-from .views import CatalogView, GoodDetailView
+from .views import CatalogView, GoodDetailView, buy_item
 
 app_name = 'catalog'
 
 urlpatterns = [
     path("", CatalogView.as_view(), name="catalog"),
-    path("item/<int:item_id>", GoodDetailView.as_view(), name="item_detail")
+    path("item/<int:item_id>", GoodDetailView.as_view(), name="item_detail"),
+    path("buy/<int:item_id>/", buy_item, name="buy_item"),
 ]
