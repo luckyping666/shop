30111687588f359f06ee93cfe73fd3948cc80dac
Roman Kuklin <kuklin.prog@gmail.com>
2026-04-24 19:18:13 +0800

    feat: add js event listener to item_card

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,5 +1,6 @@
 from os import name
 from django.urls import path
+from django.views.generic import TemplateView
 from .views import CatalogView, GoodDetailView, buy_item
 
 app_name = 'catalog'
@@ -7,5 +8,9 @@ app_name = 'catalog'
 urlpatterns = [
     path("", CatalogView.as_view(), name="catalog"),
     path("item/<int:item_id>", GoodDetailView.as_view(), name="item_detail"),
+    
     path("buy/<int:item_id>/", buy_item, name="buy_item"),
+
+    path("success/", TemplateView.as_view(template_name="catalog/success.html"), name="success"),
+    path("cancel/", TemplateView.as_view(template_name="catalog/cancel.html"), name="cancel"),
 ]
