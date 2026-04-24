b1871db99251fa5e884bf22232e99267c7d0981c
Roman Kuklin <kuklin.prog@gmail.com>
2026-04-24 20:33:19 +0800

    fix: fix item card

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
 import stripe
+from django.conf import settings
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 from django.views.generic import ListView, DetailView
@@ -18,9 +19,15 @@ class GoodDetailView(DetailView):
     context_object_name = "item"
     pk_url_kwarg = "item_id"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["STRIPE_PUBLIC_KEY"] = settings.STRIPE_PUBLISH_KEY
+        return context
+
 
 def buy_item(request, item_id):
     good = get_object_or_404(Good, id=item_id)
+    stripe.api_key = settings.STRIPE_SECRET_KEY
     session = stripe.checkout.Session.create(
         mode="payment",
         
